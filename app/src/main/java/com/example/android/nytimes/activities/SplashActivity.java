18659aa82343b4d56dc3673e8965f63d5ba263aa
Gauri Gadkari <gauri.gadkari@gmail.com>
2017-06-18 22:18:11 -0700

    Splash activity changes, removed splash activity from back stack trace

--- app/src/main/java/com/example/android/nytimes/activities/SplashActivity.java
+++ app/src/main/java/com/example/android/nytimes/activities/SplashActivity.java
@@ -18,7 +18,6 @@ public class SplashActivity extends AwesomeSplash {
 
     @Override
     public void initSplash(ConfigSplash configSplash) {
-         /* you don't have to override every property */
 
         //Customize Circular Reveal
         configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
@@ -26,21 +25,18 @@ public class SplashActivity extends AwesomeSplash {
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
 
-        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
-
         //Customize Logo
-        configSplash.setLogoSplash(R.drawable.splash_icon); //or any other drawable
+        configSplash.setLogoSplash(R.drawable.splash_icon);
 
         configSplash.setAnimLogoSplashDuration(1000); //int ms
-        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce);
 
         //Customize Title
-        configSplash.setTitleSplash("News Now");
+        configSplash.setTitleSplash(getResources().getString(R.string.app_name));
         configSplash.setTitleTextColor(R.color.white);
         configSplash.setTitleTextSize(36f); //float value
         configSplash.setAnimTitleDuration(1000);
         configSplash.setAnimTitleTechnique(Techniques.FlipInX);
-//        configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
     }
 
     @Override
